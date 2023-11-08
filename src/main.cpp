94622bdf490323080bde831a994da1caedcadfd1
Adam Flett <adam@adamflett.net>
2023-11-08 20:36:25 +0800

    simple SDL window

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,55 @@
+#include <SDL3/SDL.h>
+
 #include <iostream>
 
 int main(int argc, char* argv[]) {
-    std::cout << "Starting main."
-              << "\n";
+    if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
+        std::cerr << "Failed to initialize SDL: " << SDL_GetError()
+                  << std::endl;
+        return 1;
+    }
+
+    SDL_Window* window = SDL_CreateWindow(
+        "SDLVulk Test", 800, 600, SDL_WINDOW_RESIZABLE | SDL_WINDOW_VULKAN);
+    if (!window) {
+        std::cerr << "Failed to create window: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Renderer* renderer =
+        SDL_CreateRenderer(window, NULL, SDL_RENDERER_ACCELERATED);
+    if (!renderer) {
+        std::cerr << "Failed to create renderer: " << SDL_GetError()
+                  << std::endl;
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Event event;
+    bool m_running = true;
+    while (m_running) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_EVENT_QUIT) {
+                m_running = false;
+            }
+            if (event.type == SDL_EVENT_KEY_DOWN) {
+                auto keysympressed = event.key.keysym.sym;
+                if (event.key.keysym.sym == SDLK_ESCAPE) {
+                    m_running = false;
+                }
+            }
+        }
+
+        SDL_SetRenderDrawColor(renderer, 100, 149, 237, 255);
+        SDL_RenderClear(renderer);
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
     return 0;
 }
