7ca951aa9fc67bd4e02cbc34a8670531cb9ff7c8
Adam Flett <adam@adamflett.net>
2023-11-29 19:37:03 +0800

    buggy vulkan

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,13 @@
+
+#define VULKAN_HPP_DISPATCH_LOADER_DYNAMIC 1
+#include <vulkan/vulkan.h>
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_vulkan.h>
-#include <vulkan/vulkan.h>
 
 #include <iostream>
 #include <vector>
+
+#define VK_KHR_WIN32_SURFACE_EXTENSION_NAME "VK_KHR_win32_surface"
 
 int main(int argc, char *argv[])
 {
@@ -13,6 +17,8 @@ int main(int argc, char *argv[])
                   << std::endl;
         return EXIT_FAILURE;
     }
+
+    SDL_Vulkan_LoadLibrary(nullptr);
 
     SDL_Window *window =
         SDL_CreateWindow("SDLVulk Test", 800, 600,
@@ -36,61 +42,78 @@ int main(int argc, char *argv[])
         return EXIT_FAILURE;
     }
 
-    // VkApplicationInfo appInfo{};
-    // appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-    // appInfo.pApplicationName = "Hello Vulkan";
-    // appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
-    // appInfo.pEngineName = "No Engine";
-    // appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
-    // appInfo.apiVersion = VK_API_VERSION_1_0;
+    ////////////////////// VULKAN
 
-    // uint32_t extensionCount = 0;
-    // std::vector<const char *> extensions(extensionCount);
-    // if (!SDL_Vulkan_GetInstanceExtensions(&extensionCount))
-    // {
-    //     std::cerr << "Failed to get Vulkan instance extensions: "
-    //               << SDL_GetError() << std::endl;
-    //     SDL_DestroyWindow(window);
-    //     SDL_Quit();
-    //     return EXIT_FAILURE;
-    // }
+    // Vulkan Initialization
+    VkApplicationInfo appInfo{};
+    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    appInfo.pApplicationName = "Hello Vulkan";
+    appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.pEngineName = "No Engine";
+    appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.apiVersion = VK_API_VERSION_1_0;
 
-    // VkInstanceCreateInfo createInfo{};
-    // createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
-    // createInfo.pApplicationInfo = &appInfo;
-    // createInfo.enabledExtensionCount = extensionCount;
-    // createInfo.ppEnabledExtensionNames = extensions.data();
+    uint32_t extensionCount = 0;
+    if (!SDL_Vulkan_GetInstanceExtensions(&extensionCount))
+    {
+        std::cerr << "Failed to get Vulkan instance extensions count: " << SDL_GetError() << std::endl;
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return EXIT_FAILURE;
+    }
 
-    // VkInstance instance;
-    // if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS)
-    // {
-    //     std::cerr << "Failed to create Vulkan instance!" << std::endl;
-    //     SDL_DestroyWindow(window);
-    //     SDL_Quit();
-    //     return EXIT_FAILURE;
-    // }
+    std::vector<const char *> extensions(extensionCount);
+    if (!SDL_Vulkan_GetInstanceExtensions(&extensionCount))
+    {
+        std::cerr << "Failed to get Vulkan instance extensions: " << SDL_GetError() << std::endl;
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return EXIT_FAILURE;
+    }
 
-    // VkAllocationCallbacks allocator = {};
+    // Add the VK_KHR_win32_surface extension
+    extensions.push_back(VK_KHR_WIN32_SURFACE_EXTENSION_NAME);
 
-    // VkSurfaceKHR surface;
-    // if (!SDL_Vulkan_CreateSurface(window, instance, &allocator, &surface))
-    // {
-    //     std::cerr << "Failed to create Vulkan surface: " << SDL_GetError()
-    //               << std::endl;
-    //     vkDestroyInstance(instance, nullptr);
-    //     SDL_DestroyWindow(window);
-    //     SDL_Quit();
-    //     return EXIT_FAILURE;
-    // }
+    VkInstanceCreateInfo createInfo{};
+    createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    createInfo.pApplicationInfo = &appInfo;
+    createInfo.enabledExtensionCount = static_cast<uint32_t>(extensions.size());
+    createInfo.ppEnabledExtensionNames = extensions.data();
 
-    // float vertices[] = {
-    //     -0.5f, -0.5f, 1.0f,
-    //     0.5f, -0.5f, 1.0f,
-    //     0.5f, 0.5f, 1.0f,
-    //     -0.5f, 0.5f, 1.0f};
+    VkInstance instance;
+    std::cout << "Creating Vulkan instance..." << std::endl;
+    if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS)
+    {
+        std::cerr << "Failed to create Vulkan instance!" << std::endl;
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return EXIT_FAILURE;
+    }
+    std::cout << "Vulkan instance created successfully." << std::endl;
 
-    // vkDestroySurfaceKHR(instance, surface, nullptr);
-    // vkDestroyInstance(instance, nullptr);
+    // Create Vulkan surface
+    VkAllocationCallbacks allocator = {};
+    VkSurfaceKHR surface;
+    if (!SDL_Vulkan_CreateSurface(window, instance, &allocator, &surface))
+    {
+        std::cerr << "Failed to create Vulkan surface: " << SDL_GetError() << std::endl;
+        vkDestroyInstance(instance, nullptr);
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return EXIT_FAILURE;
+    }
+
+    // Create Vulkan device and swap chain
+    // ... (this part is missing, it involves querying for physical device, creating logical device, swap chain, etc.)
+
+    // Vertex data for a square
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f, 1.0f,
+        0.5f, -0.5f, 0.0f, 1.0f,
+        0.5f, 0.5f, 0.0f, 1.0f,
+        -0.5f, 0.5f, 0.0f, 1.0f};
+
+    // ... (Vulkan buffer creation, shader compilation, etc.)
 
     SDL_Event event;
     bool m_running = true;
